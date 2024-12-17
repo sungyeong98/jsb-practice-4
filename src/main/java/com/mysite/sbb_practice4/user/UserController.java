def7da31e73131fa9d93b5c1c53df8ebb442ccfb
sungyeong98 <am25227903@gmail.com>
2024-12-17 11:41:44 +0900

    feat: 3-06

--- src/main/java/com/mysite/sbb_practice4/user/UserController.java
+++ src/main/java/com/mysite/sbb_practice4/user/UserController.java
@@ -1,8 +1,16 @@
 package com.mysite.sbb_practice4.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @RequestMapping("/user")
 @RequiredArgsConstructor
@@ -11,6 +19,34 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm) {
+        return "signup_form";
+    }
 
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect", "error");
+            return "signup_form";
+        }
+
+        try {
+            userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+        } catch (DataIntegrityViolationException e) {
+            bindingResult.rejectValue("username", "usernameExist", "error");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("error", e.getMessage());
+            return "signup_form";
+        }
+
+        return "redirect:/";
+    }
 
 }
--- src/main/java/com/mysite/sbb_practice4/user/UserService.java
+++ src/main/java/com/mysite/sbb_practice4/user/UserService.java
@@ -2,6 +2,7 @@ package com.mysite.sbb_practice4.user;
 
 import com.mysite.sbb_practice4.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,9 +12,19 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String email, String password) {
+        SiteUser siteUser = new SiteUser();
+        siteUser.setUsername(username);
+        siteUser.setEmail(email);
+        siteUser.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(siteUser);
+        return siteUser;
+    }
 
     public SiteUser getUser(String username) {
-        Optional<SiteUser> siteUser = this.userRepository.findByUsername(username);
+        Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
         if (siteUser.isPresent()) {
             return siteUser.get();
         } else {
